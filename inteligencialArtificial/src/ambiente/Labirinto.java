be82c5b7a6b73a03759beccd03d6a89af0d13eeb
T1508BRN <t1508brn@prestadorcbmp.com.br>
2019-05-22 16:31:00 -0300

    criando um agente reativo simples - add ambiente

---
+++ inteligencialArtificial/src/ambiente/Labirinto.java
@@ -0,0 +1,38 @@
+package ambiente;
+
+public class Labirinto {
+	private int tamanholabirinto;
+	private String[][] labirinto;
+	
+	
+	/* valores
+	 * S - sujo
+	 * L - limpo
+	 * A - agente
+	 */
+	
+	public Labirinto(int tamanholabirinto) {
+		this.tamanholabirinto = tamanholabirinto;
+		this.construirlabirinto();
+	}
+	
+	
+	private void construirlabirinto() {
+		this.labirinto = new String[this.tamanholabirinto][this.tamanholabirinto];
+		for (int i = 0; i < this.tamanholabirinto; i++) {
+			for (int j = 0; j < this.tamanholabirinto; j++) {
+				this.labirinto[i][j] = "S";				
+			}			
+		}		
+	}
+
+	public void exibirLabirinto() {
+		for (int i = 0; i < this.tamanholabirinto; i++) {
+			for (int j = 0; j < this.tamanholabirinto; j++) {
+				System.out.print("| " +this.labirinto[i][j]+ " |");				
+			}
+			System.out.println("");
+		}		
+	}
+	
+}
